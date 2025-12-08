40397a65654d155eb959d65c6ca6b8a2412916ea
Thomas Armstrong <thomasarmstrong174@gmail.com>
2025-12-08 16:54:35 -0500

    Added better metrics tracking, a decent saved model, and confusion matrix generation

--- README.md
+++ README.md
@@ -5,11 +5,19 @@ To run, I've been using: `uvicorn main:app --reload`
 An example input to the training endpoint:
 ```
 {
-  "dataset_path": "/home/user/jhu/mlops/teamproject/data/initial",
+  "dataset_path": "/home/user/jhu/mlops/AI_MLOps_Team1_Project/data/initial",
   "test_size": 0.2,
-  "batch_size": 32,
+  "batch_size": 8,
+  "learning_rate": 0.001,
+  "momentum": 0.9,
   "num_epochs": 10,
-  "save_path": "/home/user/jhu/mlops/teamproject/models/test_1",
-  "model_type": "cnn"
+  "save_path": "/home/user/jhu/mlops/AI_MLOps_Team1_Project/models/test_1",
+  "model_type": "cnn",
+  "confusion_matrix_save_path": "/home/user/jhu/mlops/AI_MLOps_Team1_Project/test_1_cm.png"
 }
 ```
+
+
+Docker:
+I've been using:
+`docker build . -f Dockerfile -t mlops_teamproject:latest` and `docker run -p 8000:8000 mlops_teamproject:latest`, although the docker run will require attached volumes for data/models
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 import pandas as pd
 from utils import BrainTumorDataset
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from torch.utils.data import DataLoader
 from utils import CNN, NN
 import torch
@@ -13,6 +14,7 @@ import torch.nn.functional as F
 from typing import AsyncGenerator
 import torch.optim as optim
 from enum import Enum
+import matplotlib.pyplot as plt
 
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -26,9 +28,12 @@ class TrainRequest(BaseModel):
     dataset_path: str = Field(description="The path to the dataset on the host.")
     test_size: float = Field(default=0.2, description="The percentage (as a decimal) of the data to use for a test dataset.")
     batch_size: int = Field(default=32)
+    learning_rate: float = Field(default=0.001)
+    momentum: float = Field(default=0.9)
     num_epochs: int = Field(default=10)
     save_path: str = Field(description="The path to save the model to on the host. Include the model name.")
     model_type: AvailableModels = Field(default=AvailableModels.CNN)
+    confusion_matrix_save_path: str | None = None
 
 
 app = FastAPI(
@@ -66,7 +71,7 @@ async def training_generator(request: TrainRequest) -> AsyncGenerator:
             net = NN().to(DEVICE)
 
     criterion = nn.BCELoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=request.learning_rate, momentum=request.momentum)
 
 
     for epoch in range(request.num_epochs):  # loop over the dataset multiple times
@@ -74,6 +79,9 @@ async def training_generator(request: TrainRequest) -> AsyncGenerator:
 
         net.train()
         training_loss = 0.0
+        true_positives = 0
+        false_negatives = 0
+        false_positives = 0
         for i, data in enumerate(train_dataloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -92,11 +100,31 @@ async def training_generator(request: TrainRequest) -> AsyncGenerator:
             # print statistics
             training_loss += loss.item()
 
+            true_positives += (outputs.squeeze().round() == labels.squeeze()).count_nonzero()
+            false_negatives += torch.logical_and((outputs.squeeze().round() == 0.0), (labels.squeeze() == 1.0)).count_nonzero()
+            false_positives += torch.logical_and((outputs.squeeze().round() == 1.0), (labels.squeeze() == 0.0)).count_nonzero()
+
+            # print(f"OUTPUTS: {outputs.squeeze().round()}")
+            # print(f"CASTED: {torch.logical_and((outputs.squeeze().round() == 0.0), (labels.squeeze() == 1.0)).count_nonzero()}")
+            # print(f"LABELS: {labels.squeeze()}")
+
+            # assert False
+        yield f"TRAINING---\n"
         yield f"Average Training Loss for Epoch {epoch}: {training_loss/len(train_dataset)}\n"
+        yield f"Training Accuracy for Epoch {epoch}: {true_positives/len(train_dataset)}\n"
+        yield f"Training Recall for Epoch {epoch}: {true_positives/(true_positives + false_negatives)}\n"
+        yield f"Training Precision for Epoch {epoch}: {true_positives/(true_positives + false_positives)}\n"
+
+
+        # assert False
 
         net.eval()
         validation_loss = 0.0
-        num_correct = 0
+        true_positives = 0
+        false_negatives = 0
+        false_positives = 0
+        true_y_labels = []
+        predictions = []
         with torch.no_grad():
             for i, data in enumerate(validation_dataloader, 0):
                 inputs, labels = data
@@ -107,15 +135,30 @@ async def training_generator(request: TrainRequest) -> AsyncGenerator:
                 loss = criterion(outputs.squeeze(), labels.squeeze())
                 validation_loss += loss
 
-                num_correct += (outputs.squeeze().round() == labels.squeeze()).count_nonzero()
+                true_positives += (outputs.squeeze().round() == labels.squeeze()).count_nonzero()
+                false_negatives += torch.logical_and((outputs.squeeze().round() == 0.0), (labels.squeeze() == 1.0)).count_nonzero()
+                false_positives += torch.logical_and((outputs.squeeze().round() == 1.0), (labels.squeeze() == 0.0)).count_nonzero()
 
+                true_y_labels.extend(labels.tolist())
+                predictions.extend(outputs.round().tolist())
+
+        yield f"VALIDATION---\n"
         yield f"Average Validation Loss for Epoch {epoch}: {validation_loss/len(validation_dataset)}\n"
-        yield f"Validation Accuracy for Epoch {epoch}: {num_correct/len(validation_dataset)}\n"
+        yield f"Validation Accuracy for Epoch {epoch}: {true_positives/len(validation_dataset)}\n"
+        yield f"Validation Recall for Epoch {epoch}: {true_positives/(true_positives + false_negatives)}\n"
+        yield f"Validation Precision for Epoch {epoch}: {true_positives/(true_positives + false_positives)}\n"
 
 
         # Saving at every epoch - we can change this if we want later.
         torch.save(net.state_dict(), request.save_path)
         yield f"Saving to: {request.save_path}\n"
+    
+    
+        if epoch+1 >= request.num_epochs:
+            cm = confusion_matrix(predictions, true_y_labels)
+            disp = ConfusionMatrixDisplay(cm)
+            disp.plot()
+            plt.savefig(request.confusion_matrix_save_path)
 
 
 
